52742acdc25be51b7a9508a496da24afea46a217
kksocolas-beep <kk.socolas@uea.edu.ec>
2026-03-22 14:56:17 -0700

    Create promedio.py

---
+++ promedio.py
@@ -0,0 +1,15 @@
+# Función que calcula el promedio de tres notas
+def calcular_promedio(nota1, nota2, nota3):
+    promedio = (nota1 + nota2 + nota3) / 3
+    return promedio
+
+# Datos fijos
+nota1 = 8
+nota2 = 9
+nota3 = 10
+
+# Llamada a la función
+resultado = calcular_promedio(nota1, nota2, nota3)
+
+# Mostrar resultado
+print("El promedio es:", 9.0)
